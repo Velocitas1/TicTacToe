a204aa492f23d0db79789d6377440f972d3af5d1
Kelly Thompson <velocitas@gmail.com>
2024-03-12 20:30:55 -0500

    Initial Commit

---
+++ TicTacToe/Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace TicTacToe
+{
+    public class Game
+    {
+        private Board Board { get; } = new();
+        private Player Player1 { get; } = new();
+        private Player Player2 { get; } = new();
+        public void Start()
+        {
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+
+        }
+    }
+
+    public class Player
+    {
+
+    }
+
+    public class Board
+    {
+
+    }
+
+    public class Square
+    {
+    }
+}
